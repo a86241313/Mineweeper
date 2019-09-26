fa49a1f6f2d9662bb34fb2daf614c90de9c4fa69
a86241313 <52901866+a86241313@users.noreply.github.com>
2019-09-26 18:01:10 +0800

    190926

---
+++ src/components/navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav class="navbar navbar-dark bg-dark">
+      <b><a class="navbar-brand" href="#">
+        <img src="../assets/icon.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        Mineweeper Online
+      </a></b>
+      <div>
+           </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
